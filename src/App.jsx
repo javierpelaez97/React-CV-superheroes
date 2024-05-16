24fa11723825164f9293130b5ef1a1756baf7a27
javierpelaez97 <javierpelaezgimenez@gmail.com>
2024-05-16 19:50:25 +0200

    Página terminada

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,40 @@ import About from "./components/about/About"
 import Experience from './components/experience/Experience';
 import More from './components/more/More';
 
+
 import {CV} from "./CV/CV"
 
 const {hero, education, experience, languages, habilities, volunteer } = CV  /* Aqui hemos hecho un destructuring del componenete CV para tener el código mas limpio */
 
 function App() {
+  const [showEducation, setShowEducation] = useState(true)
   return (
     
-      <div>
+      <div className='background'>
         <Hero hero={hero}></Hero>
         <About hero = {hero.aboutMe}></About>
-        <button>Education</button>
-        <button>Experience</button>
-        <Education education ={education}></Education>
-        <Experience experience = {experience}></Experience>
+        <div className='custom-btn'>
+          
+          <button 
+            className=' btn-4'
+            onClick={()=> setShowEducation(true)}
+            >Education
+            </button>
+              <button
+              className=' btn-4'
+              onClick={()=> setShowEducation(false)}>Experience</button>
+        </div>
+        
+        <div>
+          {showEducation ? (
+            <Education education ={education}></Education>
+          ):(
+            <Experience experience = {experience}></Experience>
+          )}
+          
+        </div>
+        
+        
         <More
         languages={languages}
         habilities={habilities}
--- src/components/more/More.jsx
+++ src/components/more/More.jsx
@@ -1,18 +1,51 @@
+import "./More.css"
+
 
 const More = ({languages, habilities, volunteer})=>{
     return(
-        <div>
+        <div className="title">
             <h2>Languages</h2>
                 <div className="card">
                     <p>{languages.language}</p>
-                    <p> Writing {languages.wrlevel}</p>
-                    <p> Speaking {languages.splevel}</p>
+                    <p> ✏️ Writing {languages.wrlevel}</p>
+                    <p> 💬 Speaking {languages.splevel}</p>
                 </div>
-            <h2>Skills</h2>
-                <div>
-                    <p>{habilities}</p>
-                </div>
+            <div className="title">
+                <h2>Skills</h2>
+                    <div className="card">
+                    
+                            {habilities.map((item)=>{
+                                return(
+                                    <div key={JSON.stringify(item)}>
+                                        <p>🔧{item}</p>
+                                    </div>
+                                )
+                        })}
+                    </div>
+            </div>
+            
+            <div className="title">
+                <h2>Volunteer</h2>
+                    <div className="card">
+                    
+                    {volunteer.map((item)=>{
+                        return(
+                            <div key = {JSON.stringify(item)}>
+                            <p>🥽{item.name}</p>
+                            <p>{item.where}</p>
+                            <p>{item.description}</p>
+                        </div>
+                        )
+                    })}      
+                    </div> 
+            </div>
+              
+            
+                
+                    
+                    
         </div>
+        
     )
 
 
